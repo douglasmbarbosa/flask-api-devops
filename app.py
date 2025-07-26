84fc23a95831bfbbd119c1cd6ae22c7bd12e6041
douglasmbarbosa <douglasbarbosa081@gmail.com>
2025-07-26 12:39:30 -0300

    feat: created connection with mongodb
    feat: created connection with mongodb

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
 api = Api(app)
+db = MongoEngine()
+
+app.config["MONGODB_SETTINGS"] = [
+    {
+        "db": "users",
+        "host": "mongodb",
+        "port": 27017,
+        "username": "root",
+        "password": "123abc"
+    }
+]
+db.init_app(app)
 
 
-class HelloWorld(Resource):
+class user_model(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.StringField(required=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    birth_date = db.DateTimeField(required=True)
+
+
+class get_users(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return jsonify(user_model.objects())
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(get_users, '/users')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
